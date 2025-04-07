3959c556649cde10d2e45041abea95e849238685
holasoymalva <alquimista3301@gmail.com>
2025-04-06 22:40:40 -0600

    Initial Commit

---
+++ microJournal Watch App/microJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  microJournalApp.swift
+//  microJournal Watch App
+//
+//  Created by malva on 06/04/25.
+//
+
+import SwiftUI
+
+@main
+struct microJournal_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ microJournal Watch AppTests/microJournal_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  microJournal_Watch_AppTests.swift
+//  microJournal Watch AppTests
+//
+//  Created by malva on 06/04/25.
+//
+
+import Testing
+@testable import microJournal_Watch_App
+
+struct microJournal_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
